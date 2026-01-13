9218292488f43b9bc700af9aafc883a58f1274ae
dancingarist <dancingarist@gmail.com>
2026-01-13 16:51:42 +0900

    Update a.py

--- a.py
+++ a.py
@@ -6,9 +6,21 @@ if st.button("押してね"):
     st.write("ボタンが押されました！")
 
 CSV_FILE = "story.csv"
+all_rows = []
+
+try:
+    with open(CSV_FILE, newline="", encoding="utf-8") as f:
+        reader = csv.DictReader(f)
+        for row in reader:
+            all_rows.append(row)
+except FileNotFoundError:
+    st.error("CSVファイルが見つかりません。")
+    st.stop()
+
 
 if st.button("しね"):
     st.write("ボタンが押されました！")
+
 
 
 
